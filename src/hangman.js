f2dd2428a3f77cf24203e01beba0bbb6ca4d889a
Pawel Laskowski <pawel-laskowski@outlook.com>
2019-03-20 19:19:17 +0100

    Add readme

--- src/hangman.js
+++ src/hangman.js
@@ -48,7 +48,6 @@ class Hangman {
         } else {
             this.status = 'failed'
         }
-        console.log(this.status);
     }
 
     get statusMessage() {
@@ -63,74 +62,3 @@ class Hangman {
 }
 
 export {Hangman as default}
-
-
-
-
-
-
-// const Hangman = function (word, remainingGuesses) {
-//     this.word = word.toLowerCase().split('')
-//     this.remainingGuesses = remainingGuesses
-//     this.guessedLetters = []
-//     this.status = 'playing'
-
-//     this.getPuzzle = function () {
-//         let guessedWord = ''
-//         this.word.forEach(letter => {
-//             if (this.guessedLetters.includes(letter) || letter === ' '){
-//                 guessedWord += letter
-//             } else {
-//                 guessedWord += '*'
-//             }
-//         })
-//         return guessedWord
-        
-//     }
-// }
-
-// Hangman.prototype.makeGuess = function (char) {
-//     if (!gameOne.status === 'playing') {
-//         return
-//     }
-//     char = char.toLowerCase()
-//     if (this.guessedLetters.includes(char)) {
-//         console.log('That letter is already guessed');
-        
-//     } else {
-//         this.guessedLetters.push(char)
-//         if (!this.word.includes(char)) {
-//             this.remainingGuesses --     
-//         }          
-//     }
-
-//     this.checkStatus() 
-// }
-
-// Hangman.prototype.checkStatus = function () {
-//     let finished = this.word.every((letter) => this.guessedLetters.includes(letter))
-
-//     if (this.remainingGuesses >0){
-//         if (finished){
-//             this.status = 'finished'
-//         } else {
-//             this.status = 'playing'    
-//         }
-//     } else {
-//         this.status = 'failed'
-//     }
-//     console.log(this.status);
-// }
-
-// Hangman.prototype.getStatusMessage = function () {
-//     if (this.status === 'playing') {
-//         return `Guesses left: ${this.remainingGuesses}`
-//     } else if (this.status === 'failed') {
-//         return `Nice try! The word was "${this.word.join('')}".`
-//     } else if (this.status === 'finished') {
-//         return 'Great work! You guessed the word.'
-//     }
-// }
-
-
-
--- src/requests.js
+++ src/requests.js
@@ -1,35 +1,3 @@
-// const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
-//     const request = new XMLHttpRequest()
-
-//     request.addEventListener('readystatechange', (e) => {
-//         if (e.target.readyState === 4 && e.target.status === 200) {
-//             const data = JSON.parse(e.target.responseText)
-//             resolve(data.puzzle)
-//         } else if (e.target.readyState === 4) {
-//             reject('An error has taken place')
-//         }
-//     })
-
-//     request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
-//     request.send() 
-// })
-
-
-
-// const getPuzzleOLD = (wordCount) => {
-//     return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`, {}).then((response) => {
-//         if (response.status === 200) {
-//             return response.json()
-//         } else {
-//             throw new Error('Unable to fetch puzzle');
-//         }
-//     }).then((data) => {
-//         return data.puzzle
-//     })
-// }
-
-
-
 const getPuzzle = async (wordCount) => {
     const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`, {})
     
@@ -40,22 +8,6 @@ const getPuzzle = async (wordCount) => {
         throw new Error('Unable to get puzzle');
     }
 }
-
-// const getCountry = (countryCode) => new Promise((resolve, reject) => {
-//     const request = new XMLHttpRequest()
-//     request.addEventListener('readystatechange', (e) => {
-//         if (e.target.readyState === 4 && e.target.status === 200) {
-//             const data = JSON.parse(e.target.responseText)
-//             const country = data.find((country) => country.alpha2Code === countryCode)
-//             resolve(country.name)
-//         } else if (e.target.readyState === 4) {
-//             reject('Unable to fetch data')
-//         }
-//     })
-
-//     request.open('GET', 'http://restcountries.eu/rest/v2/all')
-//     request.send()
-// })
 
 const getCountry = async (countryCode) => {
     const response = await fetch('//restcountries.eu/rest/v2/all')
